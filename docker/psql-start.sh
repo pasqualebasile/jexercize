d1b4e5f3feb5b0aedc50ec37af4900f60c3b6a6f
Pasquale <pasquale.basile@infocube.it>
2024-04-01 17:11:06 +0200

    Reference in postgres docker command

--- docker/psql-start.sh
+++ docker/psql-start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Postgres Docker 
-#
+# https://hub.docker.com/_/postgres
 # basepath="/home/paskal/studio/dida/postgresql/"
 BASEPATH=$(dirname $(realpath "$0"))
 
 docker start psql || docker run -d --name psql -p 5432:5432 \
   -e  POSTGRES_PASSWORD=postgres \
   -v  $BASEPATH/pgdata:/var/lib/postgresql/data:rw \
-  -v  $BASEPATH/pgdump:/dump:rw  postgres:12-alpine
+  postgres:12-alpine
 
